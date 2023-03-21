8121118e9341833139819ba6d963f35db7bb3baa
cappochinho <edwardprempeh18@gmail.com>
2023-03-21 20:28:17 +0000

    test for task 0

--- 0x00-ES6_basic/0-constants.test.js
+++ 0x00-ES6_basic/0-constants.test.js
@@ -1,5 +1,5 @@
 import { taskFirst, taskNext } from './0-constants.js';
 
 test('checks if functions output expected text', () => {
-  expect(console.log(`${taskFirst()} ${taskNext()}`)).toBe('I prefer const when I can. But sometimes let is okay');
+  expect((`${taskFirst()} ${taskNext()}`)).toBe('I prefer const when I can. But sometimes let is okay');
 });
