90d3a3517dbb2f3fb489f031f99ca2160c40fe29
cappochinho <edwardprempeh18@gmail.com>
2023-03-21 20:39:24 +0000

    Final commit

--- 0x00-ES6_basic/0-constants.test.js
+++
@@ -1,5 +0,0 @@
-import { taskFirst, taskNext } from './0-constants.js';
-
-test('checks if functions output expected text', () => {
-  expect((`${taskFirst()} ${taskNext()}`)).toBe('I prefer const when I can. But sometimes let is okay');
-});
