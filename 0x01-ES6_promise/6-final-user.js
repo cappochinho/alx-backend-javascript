04fb73f617f8cadee84aa1fbbcb966ce51242f94
cappochinho <edwardprempeh18@gmail.com>
2023-03-22 23:49:24 +0000

    Submit all mandatory tasks

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise
+    .allSettled(promises)
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
