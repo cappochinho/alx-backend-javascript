3a8a52b304c494e64125940f6c93bf636151070f
Edward Jr Agyemang-Prempeh <65264688+cappochinho@users.noreply.github.com>
2023-03-23 14:01:38 +0000

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,5 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 function asyncUploader() {
-    
+  let r = {}
+  try {
+    const r_photo = uploadPhoto();
+    const r_user = createUser();
+    r = { r_photo, r_user};
+  }
+  catch (e) {
+    r = { photo: null, user: null };
+  }
+  return r;   
 }
