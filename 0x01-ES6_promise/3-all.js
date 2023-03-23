c57fd459d27aac9fae28cf81afccbaafd5a017b9
Edward Jr Agyemang-Prempeh <65264688+cappochinho@users.noreply.github.com>
2023-03-23 14:25:17 +0000

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-const photo = uploadPhoto();
-const user = createUser();
-
 function handleProfileSignup() {
-  Promise.all([photo, user])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}');
     })
-    .catch(() => (
-      'Signup system offline'
-    ));
+    .catch(() => console.log('Signup system offline'));
 }
 export default handleProfileSignup;
