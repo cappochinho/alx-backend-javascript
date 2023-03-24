aebca1a23d4aa7118a54b537baac9c7f505aa579
Edward Jr Agyemang-Prempeh <65264688+cappochinho@users.noreply.github.com>
2023-03-24 12:48:26 +0000

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,4 +2,12 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+
+  set maxStudentsSize(value) {
+    this._maxStudentsSize = value;
+  }
 }
