78be6e8b2ef282e239566564180bab7e82bafaec
cappochinho <edwardprempeh18@gmail.com>
2023-03-28 22:28:08 +0100

    Resubmitting task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,10 @@
-class Student {
-  constructor(id, firstName, location) {
-    this.id = id;
-    this.firstName = firstName;
-    this.location = location;
-  }
-}
-
-const one = new Student(1, 'Guillaume', 'San Francisco');
-const two = new Student(2, 'James', 'Columbia');
-const five = new Student(5, 'Serena', 'San Francisco');
-
 function getListStudents() {
-  return [
-    { id: one.id, firstName: one.firstName, location: one.location },
-    { id: two.id, firstName: two.firstName, location: two.location },
-    { id: five.id, firstName: five.firstName, location: five.location },
-  ];
+  const students = new Map();
+  students.set('one', { id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  students.set('two', { id: 2, firstName: 'James', location: 'Columbia' });
+  students.set('three', { id: 5, firstName: 'Serena', location: 'San Francisco' });
+
+  return [students.get('one'), students.get('two'), students.get('three')];
 }
 
 export default getListStudents;
