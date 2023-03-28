0389b92e6465785b568a26c204d34145c9135086
cappochinho <edwardprempeh18@gmail.com>
2023-03-28 23:08:09 +0100

    Added task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
-export default function getStudentsByLocation(arr, city) {
+export default function getStudentsByLocation(students, city) {
+  const filteredStudents = students.filter((student) => student.location === city);
 
+  return filteredStudents;
 }
