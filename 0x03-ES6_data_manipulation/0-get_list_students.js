54072bd87223a7d6242e9868eeddab711a9f69ac
cappochinho <edwardprempeh18@gmail.com>
2023-03-28 22:31:51 +0100

    fix 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,19 @@
-function getListStudents() {
-  const students = new Map();
-  students.set('one', { id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-  students.set('two', { id: 2, firstName: 'James', location: 'Columbia' });
-  students.set('three', { id: 5, firstName: 'Serena', location: 'San Francisco' });
-
-  return [students.get('one'), students.get('two'), students.get('three')];
+export default function getListStudents() {
+  return [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
 }
-
-export default getListStudents;
