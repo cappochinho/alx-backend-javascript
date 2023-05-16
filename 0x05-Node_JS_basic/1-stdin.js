5c70868b347476b3a6372fa8f1c9d14cba22edb3
cappochinho <edwardprempeh18@gmail.com>
2023-05-16 19:15:18 +0100

    Task 1: Fix the code to get desired output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-readline.on('line', (input) => {
-  console.log(`Your name is ${input}`);
-  console.log('This important software is now closing');
-  readline.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
