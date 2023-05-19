77dfa6bf8b6538f855294dab108516bff6555266
cappochinho <edwardprempeh18@gmail.com>
2023-05-19 21:55:03 +0100

    Task 1: ssseparated the tests into individual tests aiming to get the correct output individual operations

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,10 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  it('based on the type of operation passed', () => {
+  it('should return the sum of two rounded numbers', () => {
     assert.strictEqual(calculateNumber(3.4, 5.8, 'SUM'), 9);
+  });
+
+  it('should return the difference of two rounded numbers', () => {
     assert.strictEqual(calculateNumber(2.5, -1.2, 'SUBTRACT'), 4);
+  });
+
+  it('should return "Error" when dividing by zero', () => {
     assert.strictEqual(calculateNumber(3.4, 0.3, 'DIVIDE'), 'Error');
-    assert.strictEqual(calculateNumber(0.5, 6.3, 'DIVIDE'), (1/6));
-  })
-})
+  });
+
+  it('should return the quotient of dividing two rounded numbers', () => {
+    assert.strictEqual(calculateNumber(0.5, 6.3, 'DIVIDE'), (1 / 6));
+  });
+});
